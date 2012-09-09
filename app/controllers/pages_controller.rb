84419fe84b5914738932eaea535349ca77cff3ed
assasinbox <assasinbox@gmail.com>
2012-09-09 13:02:17 +0300

    Added dinamic content

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
 class PagesController < ApplicationController
   def home
+    @title = "HOME"
   end
 
   def contacts
+    @title = "CONTACTS"
   end
 
   def about
+    @title = "ABOUT"
   end
 end
